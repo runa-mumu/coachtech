92f4043f11947b51fdc9d0ecf4bfd5b0dcc20099
runamumu <runaruna.69.muse@gmail.com>
2021-03-11 15:54:42 +0900

    0311

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    list: "",
   },
   mutations: {
+    changeData(state, payload) {
+      state.list = payload;
+    },
   },
   actions: {
+    async changeDataAction(context) {
+      let data = await axios.get("data.json");
+      context.commit("changeData", data);
+    },
   },
-  modules: {
-  }
-})
+});
+
